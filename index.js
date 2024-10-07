c94a1ed8d43a1fabfa49af8115b485fc2ac53ac0
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-07 12:33:37 +0300

    Simple Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// Test 1
+let num1 =31;
+let num2 = 2
+let multiply = num1 * num2
+
+// Test 2
+let random = Math.floor(Math.random()*10) + 1
+
+
+// Test 3
+let num3 = 9
+let num4 = 5
+let mod = num3 % num4
+
+
+// Test 4
+let nums = [20, 8, 6, 5, 13, 14]
+let max = Math.max(...nums)
